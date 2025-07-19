9448ae0dd25745a4e8b52d757e2e415918c874c0
Jahiem Allen <justanotherinternetrandom@gmail.com>
2025-07-19 12:13:25 -0400

    changes to scraper to remove faulty data, needs clearer header, sample csv data provideds

--- src/backend/scraper/scrape.py
+++ src/backend/scraper/scrape.py
@@ -13,8 +13,6 @@ data = [el.get_text(strip=True)
 # Scraper will work fine after this point with no issues on data returns
 # However further splitting of the returned data so it's sectioned is easier formatting
 # Plus it allows for easier CSV -> JSON conversion should be we need it    
-df = pd.DataFrame(data, columns=["Content"])
-# Needs to be a line that removes duplicates from the data 
-df_cleaned = df.drop_duplicates(keep='first')   
+df = pd.DataFrame(data, columns=["Content"])   
 
-df_cleaned.to_csv("output.csv", index=False)
+df.to_csv("output.csv", index=False)
