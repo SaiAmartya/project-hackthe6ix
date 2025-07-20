8a0584bce5269cec58a27a5beea71f7ffec05a87
Amruth Arunkumar <amruth1.618@gmail.com>
2025-07-19 22:26:53 -0400

    python files for data cleaning

---
+++ scripts/vellum_shelter_fetch.py
@@ -0,0 +1,22 @@
+import pymongo
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def fetch_shelter_data():
+    try:
+        uri = os.getenv("MONGO_CONNECTION_STRING")
+        client = MongoClient(uri)
+        database = client["Info"]
+        collection = database["ShelterInfo"]
+        results = list(collection.find({}, {"_id": 0}))
+        client.close()
+    except Exception as e:
+        raise Exception(
+            "The following error occurred: ", e)
+
+    return {"shelters": results}
+
+print(fetch_shelter_data())
